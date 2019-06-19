9206ddd1dd6890954d80f9fd49d6ef9aa4051b06
slcassidy <samantha.l.cassidy@gmail.com>
2019-06-18 20:59:39 -0400

    Finally works overall

--- index.js
+++ index.js
@@ -13,37 +13,54 @@ AWS.config.update({
     region: 'us-east-2',
     endpoint: 'http://dynamodb:us-east-2:805129476280:table/tbl_movie',
     accessKeyId: `${key1}`,
+    // accessKeyID: "LTMkN8HRJlKd9sk1PTsghivlY8aVRTudlJxsM7yS",
     secretAccessKey: `${key2}`
+    // secretAccessKey: 'AKIAI4ECLURCC5QZBDAA'
 });
 
 
 exports.handler = (event, context, callback) => {
     // TODO implement
     // requesting 
-request(`https://www.omdbapi.com/?t=${event.queryStringParameters.name}&y=&plot=short&apikey=${key}`, (error, apiResponse, body) => {
-    // response below to show the information
-  const lambdaResponse = {
-    statusCode: 200,
-    // body: JSON.stringify(`${event['queryStringParameters']['name']}`)
+    request(`https://www.omdbapi.com/?t=${event.queryStringParameters.name}&y=&plot=short&apikey=${key}`, (error, apiResponse, body) => {
+        // response below to show the information
+        const lambdaResponse = {
+            statusCode: 200,
+            // body: JSON.stringify(`${event['queryStringParameters']['name']}`)
 
-    body: body,
-    headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json"
-    }
-    // ,
-    // Item: {
-    //     name: 'movie_search',
-    //     type: 'HTTP',
-    //     title: `${event.queryStringParameters.name}`,
-    //     timestamp: String(new Date().getTime())
-    // }
+            body: body,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "application/json"
+            }
+
+        };
+        // return response;
+        callback(null, lambdaResponse);
+    });
 };
-// return response;
-callback(null, lambdaResponse);
-});
 
+function addToDynamoDB(name) {
+  
     
+    // title: `${event.queryStringParameters.name}`
+    var stuff = {
+    TableName: 'tbl_movie',
+    Item: {
+        name: 'Lambda Entry',
+        type: 'HTTP',
+        title: name,
+        timestamp: String(new Date().getTime())
+    }
+}
+
+new AWS.DynamoDB.DocumentClient.put(stuff, function (err, data){
+    if (err) {
+        console.error("DOES NOT WORK");
+    } else {
+        console.log("HOORAY YOU ADDED!!!", JSON.stringify(data, null, 2));
+    }
+})
 };
 
 
